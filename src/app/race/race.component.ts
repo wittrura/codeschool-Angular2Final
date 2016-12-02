49da8d1fa0ec546f46e3e3c8e1ede63426e7b2a2
Ryan Wittrup <ryan.wittrup@gmail.com>
2016-12-01 22:45:19 -0500

    Adds race component and refactors responsibilities

---
+++ src/app/race/race.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+import { RacesComponent } from '../races/races.component';
+
+@Component({
+  selector: 'race',
+  templateUrl: './race.component.html',
+  styleUrls: ['./race.component.css']
+})
+export class RaceComponent implements OnInit {
+  @Input() race;
+
+  constructor(
+    private racesComponent: RacesComponent
+  ) { }
+
+  ngOnInit() {
+  }
+
+  castDate(date) {
+    return new Date(date);
+  }
+
+  enterRace(): void {
+    this.racesComponent.enterRace(this.race);
+  }
+
+  cancelRace(): void {
+    this.racesComponent.cancelRace(this.race);
+  }
+}
